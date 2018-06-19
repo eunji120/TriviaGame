338e71923e92142b5cda96d3c92f6566763d4522
eunji120 <eunji120@gmail.com>
2018-06-18 20:08:49 -0400

    Edited files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //set up counter timer
 var counter = 0;
-var timeleft = 30;
+var timeleft = 95;
 
 function convertSeconds(s) {
     var min = floor(s / 60);
@@ -23,6 +23,45 @@ function setup() {
 }
 
 //set up start button
-//questions
+//set up submitAnswers function
+function submitAnswers() {
+    var total = 6;
+    var score = 0;
+
+    //get user input
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+    var q6 = document.forms["quizForm"]["q6"].value;
+
+    //validation
+    
+    for(i = 1; i <= total; i++) {
+        if(eval('q'+i) == null || eval('q'+i) == '') {
+            alert('You missed question '+ i);
+            return false;
+        }
+    }
+
+//set right answers
+    var answers = ["b", "a", "a", "c", "c", "a"];
+
+//check the answers
+    for(i = 1; i <= total; i++) {
+        if(eval('q'+i) == answers[i - 1]) {
+            score++;
+        }
+    }
+
+    //Display results
+    var results = document.getElementById('results');
+    results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
+    
+    return false;
+    
+}
+
 //set up Finish button
 //directs to the results page
