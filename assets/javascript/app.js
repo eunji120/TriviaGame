87f151420fc10d4c2b6dd03e109630cb9861a83a
eunji120 <eunji120@gmail.com>
2018-06-20 21:29:10 -0400

    Edited files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,30 @@
 var counter = 0;
 var timeleft = 95;
 
-//the timer starts as soon as the page loads
-window.onload = function() {
-    $("#timer").on("click", stopwatch.recordLap);
-    $("#start").on("click", stopwatch.start);
-    $("#submit").on("click", stopwatch.stop);
-};
 
-$("#display").text("01:35");
-$("#timer").text("");
+    var stopwatch = {
+        start: function() {
+            $(document).ready(function() {
+                $("#remaining-time").hide();
+                $("#timer").on("click", stopwatch.recordLap);
+                $("#start").on("click", stopwatch.start);
+                $("#submit").on("click", stopwatch.stop);
+            });
+            
+            // $("#display").text("01:35");
+            // $("#timer").text(""); 
+        }
+    }
+
+//the timer starts as soon as the page loads
+// window.onload = function() {
+//     $("#timer").on("click", stopwatch.recordLap);
+//     $("#start").on("click", stopwatch.start);
+//     $("#submit").on("click", stopwatch.stop);
+// };
+
+// $("#display").text("01:35");
+// $("#timer").text("");
 
 // var intervalId;
 // var clockRunning = false;
@@ -42,63 +57,86 @@ $("#timer").text("");
 //     }
 // }
 
-function convertSeconds(s) {
-    var min = floor(s / 60);
-    var sec = s % 60;
-    return nf(min,2) + ':' + nf(sec,2);
-}
 
-function setup() {
-    noCanvas(); 
-
-    var timer = select('#timer');
-    timer.html(convertSeconds(timeleft - counter));
-
-    function timeIt() {
-        counter--;
-        timer.html(convertSeconds(timeleft - counter));
+    function convertSeconds(s) {
+        var min = floor(s / 60);
+        var sec = s % 60;
+        return nf(min,2) + ':' + nf(sec,2);
     }
 
-    setInterval(timeIt, 1000);
-}
+    function setup() {
+        noCanvas(); 
+
+        var timer = select('#timer');
+        timer.html(convertSeconds(timeleft - counter));
+
+        function timeIt() {
+            counter--;
+            timer.html(convertSeconds(timeleft - counter));
+        }
+
+        setInterval(timeIt, 1000);
+    }
 
 //set up submitAnswers function
-function submitAnswers() {
-    var total = 6;
-    var score = 0;
 
-    //get user input
-    var q1 = document.forms["quizForm"]["q1"].value;
-    var q2 = document.forms["quizForm"]["q2"].value;
-    var q3 = document.forms["quizForm"]["q3"].value;
-    var q4 = document.forms["quizForm"]["q4"].value;
-    var q5 = document.forms["quizForm"]["q5"].value;
-    var q6 = document.forms["quizForm"]["q6"].value;
-
-    //validation
-    
-    for(i = 1; i <= total; i++) {
-        if(eval('q'+i) == null || eval('q'+i) == '') {
-            alert('You missed question '+ i);
-            return false;
-        }
-    }
-
-//set right answers
-    var answers = ["b", "a", "a", "c", "c", "a"];
-
-//check the answers
-    for(i = 1; i <= total; i++) {
-        if(eval('q'+i) == answers[i - 1]) {
-            score++;
-        }
-    }
-
-    //Display results on separate page
-    var results = document.getElementById('results');
-    results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
-    
-    return false;
-    
+var quiz = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    total: 6,
+    score: 0,
+    timer: 95,
+    timerOn: false,
+    timerId: '',
 }
+
+
+    function submitAnswers() {
+        // var total = 6;
+        // var score = 0;
+
+        //get user input
+        var q1 = document.forms["quizForm"]["q1"].value;
+        var q2 = document.forms["quizForm"]["q2"].value;
+        var q3 = document.forms["quizForm"]["q3"].value;
+        var q4 = document.forms["quizForm"]["q4"].value;
+        var q5 = document.forms["quizForm"]["q5"].value;
+        var q6 = document.forms["quizForm"]["q6"].value;
+
+        //validation
+        
+        for(i = 1; i <= total; i++) {
+            if(eval('q'+i) == null || eval('q'+i) == '') {
+                alert('You missed question '+ i);
+                return false;
+            }
+        }
+
+    //set right answers
+        var answers = ["b", "a", "a", "c", "c", "a"];
+
+    //check the answers
+        for(i = 1; i <= total; i++) {
+            if(eval('q'+i) == answers[i - 1]) {
+                score++;
+            }
+        }
+
+  
+    
+
+        //Display results on separate page
+        $("#results").html("<h4>Results!</h4>" + 
+        "<p>Correct: "+ quiz.correct +"</p>" +
+        "<p>Incorrect: "+ quiz.incorrect +"</p>" +
+        "<p>Unanswered: "+ quiz.unanswered +"</p>");
+
+        //hide game section
+        $("#game").hide();
+        //show start button to begin a new game
+        $("#start").show();
+    }
+
+    
 
